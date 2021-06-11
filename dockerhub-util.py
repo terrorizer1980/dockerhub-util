9522aefafbbdbde94fe26b2ac56b7bb3e01e8a45
michael@dockter.com <michael@dockter.com>
2021-06-11 15:09:36 -0400

    #3 Update swagger-ui default

--- dockerhub-util.py
+++ dockerhub-util.py
@@ -18,7 +18,7 @@ from datetime import date
 __all__ = []
 __version__ = "1.0.1"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2021-02-22'
-__updated__ = '2021-04-09'
+__updated__ = '2021-06-11'
 
 SENZING_PRODUCT_ID = "5018"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
@@ -170,7 +170,7 @@ dockerhub_repositories_for_latest = {
     },
     'swagger-ui': {
         'environment_variable': 'SENZING_DOCKER_IMAGE_VERSION_SWAGGERAPI_SWAGGER_UI',
-        'version': 'latest'
+        'version': 'v3.50.0'
     },
     'web-app-demo': {
         'environment_variable': 'SENZING_DOCKER_IMAGE_VERSION_WEB_APP_DEMO'
